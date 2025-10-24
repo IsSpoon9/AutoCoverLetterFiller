ebab8db3ac3cf57a45f2d4a50dcad043cb5e2f05
Ethan Siemens <ethanhunter090@gmail.com>
2025-10-24 13:22:24 -0700

    Fixed Cmake. Got CMD prompt removed on launch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "AutoCoverLetterFiller.h"
 
+#ifdef _DEBUG
 int main() {
 	CAutoCoverLetter app;
 	int appResult = app.run();
 	return appResult;
 }
+#else
+#include "Windows.h"
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+	CAutoCoverLetter app;
+	int appResult = app.run();
+	return appResult;
+}
+#endif
